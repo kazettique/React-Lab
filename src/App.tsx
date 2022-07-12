c1ba819def9e99c8d7f7710d45a380f1564b6f8c
Woody.Chang <07632.woody.chang@ict888.net>
2022-07-12 13:16:17 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import SafariAbsoluteTest from './components/SafariAbsoluteTest';
 import { useMobileViewPortHeight } from './hooks';
+import useIsMobile from './hooks/useIsMobile';
 
 function App() {
   const mobileViewPortHeight = useMobileViewPortHeight();
+  const isMobile = useIsMobile();
 
   return (
     <div className="h-screen w-screen" style={mobileViewPortHeight.style}>
-      <SafariAbsoluteTest />
+      {/* <SafariAbsoluteTest /> */}
+      isMobile: {String(isMobile)}
+      <div className="bg-red-300 lg:bg-blue-300 w-20 h-20" />
     </div>
   );
 }
---
+++ src/hooks/useIsMobile.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const getIsMobile = () => window.innerWidth <= 1024;
+
+function useIsMobile(): boolean {
+  const [isMobile, setIsMobile] = useState(getIsMobile());
+
+  useEffect(() => {
+    const onResize = () => setIsMobile(getIsMobile());
+    window.addEventListener('resize', onResize);
+
+    onResize();
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  return isMobile;
+}
+
+export default useIsMobile;
