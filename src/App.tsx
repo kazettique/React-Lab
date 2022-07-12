52f9160d4b4051732e134c156439f95e347cf369
Woody.Chang <07632.woody.chang@ict888.net>
2022-07-12 14:14:18 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,18 @@ function App() {
   const mobileViewPortHeight = useMobileViewPortHeight();
   const isMobile = useIsMobile();
 
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+
   return (
-    <div className="h-screen w-screen" style={mobileViewPortHeight.style}>
+    <div className="h-screen w-screen p-5" style={mobileViewPortHeight.style}>
       {/* <SafariAbsoluteTest /> */}
       isMobile: {String(isMobile)}
       <div className="bg-red-300 lg:bg-blue-300 w-20 h-20" />
+      <div>
+        <div>window height: {windowHeight}</div>
+        <div>window width: {windowWidth}</div>
+      </div>
     </div>
   );
 }
