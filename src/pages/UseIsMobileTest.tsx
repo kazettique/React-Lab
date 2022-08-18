8a2bf93aacf32ba43ea5f3fe61476d2416515a76
Woody.Chang <07632.woody.chang@ict888.net>
2022-08-18 18:03:28 +0800

    update

--- src/pages/UseIsMobileTest.tsx
+++ src/pages/UseIsMobileTest.tsx
@@ -1,9 +1,11 @@
+import { useIsPortrait } from '@/hooks';
 import useIsMobile from '@/hooks/useIsMobile';
 import useIsMobile2 from '@/hooks/useIsMobile2';
 
 export default function UseIsMobileTest() {
   const isMobile = useIsMobile();
   const isMobile2 = useIsMobile2();
+  const isPortrait = useIsPortrait();
 
   return (
     <div>
@@ -14,6 +16,14 @@ export default function UseIsMobileTest() {
       <div>isMobile2</div>
       <div>
         {isMobile2 ? <span className="text-green-500">Mobile</span> : <span className="text-blue-500">Tablet</span>}
+      </div>
+      <div>isPortrait</div>
+      <div>
+        {isPortrait ? (
+          <span className="text-green-500">Portrait</span>
+        ) : (
+          <span className="text-blue-500">Landscape</span>
+        )}
       </div>
       <div>window.innerWidth</div>
       <div>{window.innerWidth}</div>
